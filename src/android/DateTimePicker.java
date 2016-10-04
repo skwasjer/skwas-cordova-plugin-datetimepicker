219722ddb99b701f41513ee0a524400f71eb99e3
skwas <skwas@skwas.net>
2016-10-04 08:48:05 +0200

    - Move ISO8601 to seperate file - Add proper support for UTC (Z) and other timezones - Add overloads for java.util.Date
    Closes #3

--- src/android/DateTimePicker.java
+++ src/android/DateTimePicker.java
@@ -1,22 +1,16 @@
 package com.skwas.cordova.datetimepicker;
 
 import java.lang.reflect.Method;
-import java.lang.IndexOutOfBoundsException;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -59,7 +53,7 @@ public class DateTimePicker extends CordovaPlugin {
 	}
 
 	public synchronized boolean show(final JSONArray data, final CallbackContext callbackContext) {
-		Calendar c = GregorianCalendar.getInstance();
+		Calendar calendar = GregorianCalendar.getInstance();
 		final Runnable runnable;
 		final Context currentCtx = cordova.getActivity();
 		final DateTimePicker datePickerPlugin = this;
@@ -78,23 +72,20 @@ public class DateTimePicker extends CordovaPlugin {
 	
 				// Get interval for time picker.
 				String sMinuteInterval = obj.getString("minuteInterval");
-				if (sMinuteInterval != null && sMinuteInterval != "")
-					minuteInterval = Integer.parseInt(sMinuteInterval);
+				if (sMinuteInterval != null && sMinuteInterval != "") {
+                    minuteInterval = Integer.parseInt(sMinuteInterval);
+                }
 	
 				// Get date/time.
 				String date = obj.getString("date");
 				if (date != null && date != "") {
 					try {
 						// Attempt to parse with ISO 8601.
-						c = ISO8601.toCalendar(date);
+						calendar = ISO8601.toCalendar(date);
 					}
 					catch (ParseException ex) {
-						// If failed, try again using MS JSON date format (returns null if not succesful).
-						c = MicrosoftJSONDate.toCalendar(date);
-						if (c == null) {
-							callbackContext.error("Failed to parse date/time: " + date);
-							return false;
-						}
+						callbackContext.error("Failed to parse date/time: " + date);
+						return false;
 					}
 				}
 	
@@ -102,7 +93,6 @@ public class DateTimePicker extends CordovaPlugin {
 				// boolean optionAllowOldDates = obj.getBoolean("allowOldDates");
 			}
 			catch (JSONException ex) {
-//				ex.printStackTrace();
 				if (obj == null)
 					callbackContext.error("Failed to load JSON options." + ex.getMessage());
 				else
@@ -113,14 +103,14 @@ public class DateTimePicker extends CordovaPlugin {
 
 
 		// Get final values.
-		final int mYear = c.get(Calendar.YEAR);
-		final int mMonth = c.get(Calendar.MONTH);
-		final int mDay = c.get(Calendar.DAY_OF_MONTH);
-		final int mHour = c.get(Calendar.HOUR_OF_DAY);
-		final int mMinutes = c.get(Calendar.MINUTE);
+		final int mYear = calendar.get(Calendar.YEAR);
+		final int mMonth = calendar.get(Calendar.MONTH);
+		final int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+		final int mHour = calendar.get(Calendar.HOUR_OF_DAY);
+		final int mMinutes = calendar.get(Calendar.MINUTE);
 		final int mMinuteInterval = minuteInterval;
 		final String sAction = action;
-		final Calendar fCalendar = c;
+		final Calendar fCalendar = calendar;
 
 
 		if (ACTION_TIME.equalsIgnoreCase(action)) {
@@ -205,7 +195,7 @@ public class DateTimePicker extends CordovaPlugin {
 			calendar.set(Calendar.YEAR, year);
 			calendar.set(Calendar.MONTH, monthOfYear);
 			calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-	
+
 			Date date = calendar.getTime();
 			Long millis = Long.valueOf(date.getTime());
 			try {
@@ -240,7 +230,7 @@ public class DateTimePicker extends CordovaPlugin {
 			calendar.set(Calendar.MINUTE, minute);
 			calendar.set(Calendar.SECOND, 0);
 			calendar.set(Calendar.MILLISECOND, 0);
-			
+
 			Date date = calendar.getTime();
 			Long millis = Long.valueOf(date.getTime());
 			try {
@@ -250,58 +240,5 @@ public class DateTimePicker extends CordovaPlugin {
 				callbackContext.error("Failed to get time.");
 			}
 		}
-	}
-	
-	private static class MicrosoftJSONDate {
-		
-		public static Calendar toCalendar(final String jsonDate) {
-			Pattern regex = Pattern.compile("/?Date\\((\\d+)(([+-]?)(\\d{2})(\\d{2}))?\\)/");
-			Matcher match = regex.matcher(jsonDate);
-			if (match.find()) {
-				Long ticks = Long.valueOf(match.group(1));
-				// TODO: implement UTC offset from groups 3, 4, 5 (+, HH, mm)
-				Calendar calendar = GregorianCalendar.getInstance();
-				calendar.setTime(new Date(ticks.longValue()));
-				return calendar;
-			}
-			return null;
-		}		
-	}
-	
-	// http://stackoverflow.com/questions/2201925/converting-iso-8601-compliant-string-to-java-util-date
-	/**
-	 * Helper class for handling a most common subset of ISO 8601 strings
-	 * (in the following format: "2008-03-01T13:00:00+01:00"). It supports
-	 * parsing the "Z" timezone, but many other less-used features are
-	 * missing.
-	 */
-	private static class ISO8601 {
-	    /** Transform Calendar to ISO 8601 string. */
-	    public static String fromCalendar(final Calendar calendar) {
-	        Date date = calendar.getTime();
-	        String formatted = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-	            .format(date);
-	        return formatted.substring(0, 22) + ":" + formatted.substring(22);
-	    }
-
-	    /** Get current date and time formatted as ISO 8601 string. */
-	    public static String now() {
-	        return fromCalendar(GregorianCalendar.getInstance());
-	    }
-
-	    /** Transform ISO 8601 string to Calendar. */
-	    public static Calendar toCalendar(final String iso8601string)
-	            throws ParseException {
-	        Calendar calendar = GregorianCalendar.getInstance();
-	        String s = iso8601string.replace("Z", "+00:00");
-	        try {
-	            s = s.substring(0, 22) + s.substring(23);  // to get rid of the ":"
-	        } catch (IndexOutOfBoundsException e) {
-	            throw new ParseException("Invalid length", 0);
-	        }
-	        Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(s);
-	        calendar.setTime(date);
-	        return calendar;
-	    }
 	}
 }
