ad4eea5c19331745fa99ddc17a83dfa17b444b61
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2018-05-27 01:48:30 +0300

    Add typings for use with typescript

---
+++ types/index.d.ts
@@ -0,0 +1,52 @@
+/**
+ * Interface for cordova.plugins adding DateTimePicker
+ *
+ * @interface Plugins
+ */
+interface Plugins {
+  DateTimePicker: DateTimePicker;
+}
+
+interface IDatePickerOptions {
+  mode: 'date' | 'time' | 'datetime';
+  date: Date;
+  minDate?: Date;
+  maxDate?: Date;
+  allowOldDates?: boolean;
+  allowFutureDates?: boolean;
+  minuteInterval?: number;
+  locale?: string;
+  okText?: string;
+  cancelText?: string;
+  android?: {
+    theme?: number; // Theme_DeviceDefault_Dialog
+    calendar?: boolean;
+    is24HourView?: boolean;
+  };
+  success: (newDate: Date) => void;
+  cancel?: () => void;
+  error: (err: Error) => void;
+}
+
+interface DateTimePicker {
+
+  /**
+   * Show the plugin with specified options.
+   *
+   * @param {IDatePickerOptions} options
+   * @memberof DateTimePicker
+   */
+  show(options: IDatePickerOptions): void;
+
+  /**
+   * Show the plugin with specified options and callbacks.
+   * Legacy way to call the show method, kept for backward compatibility.
+   * NOTE: The successCallback and errorCallback respectively will be ignored if the success or error callback is provided on the options argument.
+   *
+   * @param {IDatePickerOptions} options
+   * @param {(newDate: Date) => void} successCb
+   * @param {(err: Error) => void} errorCb
+   * @memberof DateTimePicker
+   */
+  show(options: IDatePickerOptions, successCb: (newDate: Date) => void, errorCb: (err: Error) => void): void;
+}
---
+++ types/typings-test.ts
@@ -0,0 +1,40 @@
+// tslint:disable-next-line:no-reference
+/// <reference path="index.d.ts" />
+
+declare var cordova: { plugins: Plugins };
+
+const dateTimePicker: DateTimePicker = cordova.plugins.DateTimePicker;
+
+const optionsFull: IDatePickerOptions = {
+  mode: 'datetime',
+  date: new Date(),
+  minDate: new Date(),
+  maxDate: new Date(),
+  allowOldDates: false,
+  allowFutureDates: true,
+  minuteInterval: 5,
+  locale: 'en_GB',
+  okText: 'Select',
+  cancelText: 'Cancel',
+  android: {
+    theme: 5, // Theme_DeviceDefault_Dialog
+    calendar: false,
+    is24HourView: true
+  },
+  success: (newDate: Date) => { /* newDate */ },
+  cancel: () => { /* cancelled */ },
+  error: (err: Error) => { /* error */ }
+
+};
+dateTimePicker.show(optionsFull);
+
+const optionsMin: IDatePickerOptions = {
+  mode: 'date',
+  date: new Date(),
+  success: (newDate: Date) => { /* newDate */ },
+  error: (err: Error) => { /* error */ }
+};
+dateTimePicker.show(optionsMin);
+
+// override method
+dateTimePicker.show(optionsMin, (newDate) => { /* newDate */ }, (err) => { /* error */ });
