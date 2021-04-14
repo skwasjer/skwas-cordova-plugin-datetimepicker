b7a6b66449f293b998551588b30d73a611acbc7e
Echonox <angelkomyou@gmail.com>
2021-04-14 11:05:38 +0200

    AndroidX Support

--- src/android/TimePickerDialog.java
+++ src/android/TimePickerDialog.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.TextUtils;
 import android.widget.DatePicker;
 import android.widget.NumberPicker;
